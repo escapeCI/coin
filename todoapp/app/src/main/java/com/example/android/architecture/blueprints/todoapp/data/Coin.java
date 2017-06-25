a029416244e1021e7c8f3f752725ecbf2c4dd343
rolroralra <rolroralra@gmail.com>
2017-06-25 21:06:29 +0900

    first commit

---
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/Coin.java
@@ -0,0 +1,11 @@
+package com.example.android.architecture.blueprints.todoapp.data;
+
+/**
+ * Created by Shinyoung.Kim on 2017-06-25.
+ */
+
+public class Coin {
+    private String name;
+    private Exchange exchange;
+    private PriceInfo priceInfo;
+}
---
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/PriceInfo.java
@@ -0,0 +1,10 @@
+package com.example.android.architecture.blueprints.todoapp.data;
+
+/**
+ * Created by Shinyoung.Kim on 2017-06-25.
+ */
+
+public class PriceInfo {
+    double currPrice;
+    double avgPrice24H;
+}
